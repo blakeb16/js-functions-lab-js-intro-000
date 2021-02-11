8ed3f342e019d5748ffaa3e8e2caaeb7667ccad2
Blake Busby <blakebusby16@gmail.com>
2021-02-11 00:28:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ var name = "everyone"
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
+
+var holiday = "whatever day"
+var name = "pal"
+function happyCustomHolidayTo(holiday,name){
+  return `Happy ${holiday}, ${name}!`
+}
